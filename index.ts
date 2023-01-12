8964555741ad958493f949d9a28146f44274c0f8
Дмитрий Попков <dmitriypopkov98@gmail.com>
2023-01-12 20:57:17 +0300

    try

--- index.ts
+++ index.ts
@@ -20,8 +20,7 @@ app.use(cookieParser());
 
 app.use(bodyParser.json());
 
-app.use(express.static(path.join(__dirname, "public")));
-console.log(path.resolve(__dirname + "/client/public"));
+app.use("/static", express.static(path.join(__dirname, "public")));
 
 app.use("/auth", authRouter);
 app.use("/cards", cardsRouter);
